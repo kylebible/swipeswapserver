6b6e7f8c726efd0e17eb6a553c16a15e73ccf760
KYLE C BIBLE <kylecbible@KYLEs-MacBook-Pro.local>
2017-06-01 18:49:06 -0700

    modified settings

--- swipeswap/settings.py
+++ swipeswap/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '4h7psqvhbio%p17a1^%p9tr_j#50!)-=ozvw-a^1541*my-yut'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://lit-ocean-33218.herokuapp.com/']
 
 
 # Application definition
